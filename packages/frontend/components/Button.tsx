bb5faaebed7aa7fbbdaf83d714daa6ac14352536
zhwltlr <vyoungv7340@naver.com>
2024-02-19 21:53:26 +0900

    run sb

---
+++ packages/frontend/components/Button.tsx
@@ -0,0 +1,20 @@
+import React from "react";
+import PropTypes from "prop-types";
+
+const Button = ({ onClick, children }) => {
+  return (
+    <button
+      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded"
+      onClick={onClick}
+    >
+      {children}
+    </button>
+  );
+};
+
+Button.propTypes = {
+  onClick: PropTypes.func.isRequired,
+  children: PropTypes.node.isRequired,
+};
+
+export default Button;
